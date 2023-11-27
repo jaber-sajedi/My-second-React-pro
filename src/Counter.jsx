0a7e3a99de72e09fcc6942e82daa719c04fa3ba0
jaber-sajedi <83576757+jaber-sajedi@users.noreply.github.com>
2023-11-27 13:40:11 +0330

    The commit adds an attack button and a defend button to the Counter component.
    The attack button increments the count by 1 and the defend button decrements the count by 1.
    
    The commit also changes the header text to "CountOpedia". (generated by blackbox.ai )

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,32 +1,102 @@
 import React from "react";
-
+import attack from "./Images/attack.png";
+import defend from "./Images/defend.png";
 export default class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.handleAttack = this.handleAttack.bind(this);
     this.handleDefence = this.handleDefence.bind(this);
-    this.state={
-      count:0
-    }
+    this.state = {
+      count: 0,
+    };
   }
-  handleAttack() {
+
+  handleAttack = () => {
     //alert("Attack clicked");
-    this.setState({count:this.state.count+1})
-  }
-   handleDefence() {
-     //alert("Defend clicked");
-     this.setState({count:this.state.count-1})
-  }
+    // this.setState({ count: this.state.count + 1 });
+
+    this.setState((perviousState) => {
+      //یک عدد را ده ضرب می کند و گرد میکند
+      let newCount = perviousState.count + Math.round(Math.random() * 10);
+      return {
+        count: newCount,
+      };
+    });
+  };
+
+  handleDefence = () => {
+    //alert("Defend clicked");
+    this.setState((perviousState) => {
+      let newCount = perviousState.count - Math.round(Math.random() * 10);
+      return {
+        count: newCount,
+      };
+    });
+  };
+
+  handleRandomPlay = () => {
+    let playMode = Math.round(Math.random());
+    if (playMode == 0) {
+      this.handleAttack();
+    } else {
+      this.handleDefence();
+    }
+  };
+
+  handleReset = () => {
+    this.setState(() => {
+      return {
+        count: 0,
+      };
+    });
+  };
 
   render() {
     return (
-      <div className="row text-white " >
-        <h1 >Counter :{this.state.count}</h1>
-        <button onClick={this.handleAttack} style={{width:"200px"}}>+1</button>
-        <button onClick={this.handleDefence} style={{width:"200px"}}>-1</button>
+      <div className="row text-white text-center">
+        <h1>Game Score :{this.state.count}</h1>
+        <p>you win at +10 point and lose at -10 points!</p>
+        <p>Last Play</p>
+        <h3>Game State :</h3>
+
+        <div className="col-6 col-md-3 offset-md-3">
+          <img
+            alt=""
+            style={{
+              width: "100px",
+              cursor: "pointer",
+              border: "1px solid green",
+            }}
+            className="p-4 rounded"
+            src={attack}
+            onClick={this.handleAttack}
+          />
+        </div>
+
+        <div className="col-6 col-md-3  ">
+          <img
+            alt=""
+            style={{
+              width: "100px",
+              cursor: "pointer",
+              border: "1px solid red",
+            }}
+            className="p-4 rounded"
+            src={defend}
+            onClick={this.handleDefence}
+          />
+        </div>
+        <div className="col-12 col-md-4 offset-md-4 p-4">
+          <button
+            className="btn btn-secondary w-100 mt-2"
+            onClick={this.handleRandomPlay()} >
+            Random Play</button>
+          <br />
+
+          <button className="btn btn-warning w-100 mt-2"
+             onClick={this.handleReset()} >Reset Play</button>
+        </div>
       </div>
     );
   }
 }
-
- 
