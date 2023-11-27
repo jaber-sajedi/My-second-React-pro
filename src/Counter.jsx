4dccfda24d12b80897265bb7d7ed3dd07de4c1ef
jaber-sajedi <83576757+jaber-sajedi@users.noreply.github.com>
2023-11-27 09:18:05 +0330

    test

---
+++ src/Counter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAttack = this.handleAttack.bind(this);
+    this.handleDefence = this.handleDefence.bind(this);
+    this.state={
+      count:0
+    }
+  }
+  handleAttack() {
+    //alert("Attack clicked");
+    this.setState({count:this.state.count+1})
+  }
+   handleDefence() {
+     //alert("Defend clicked");
+     this.setState({count:this.state.count-1})
+  }
+
+  render() {
+    return (
+      <div className="row text-white " >
+        <h1 >Counter :{this.state.count}</h1>
+        <button onClick={this.handleAttack} style={{width:"200px"}}>+1</button>
+        <button onClick={this.handleDefence} style={{width:"200px"}}>-1</button>
+      </div>
+    );
+  }
+}
+
+ 
---
+++ src/Layout/Header.jsx
@@ -0,0 +1,12 @@
+import logo from "../Images/react.png";
+ function Header()
+{
+    return(
+     <div className="py-2 pl-2" style={{borderBottom:"1px solid #777"}}>
+        <img alt="" src={logo} style={{width:"36px",height:"36px" ,padding:"5px"}}/>
+        <span className="h2 pt-4 text-white-50" >React Course - CountOpedia</span>
+     </div>
+    )
+}
+
+export default Header;
